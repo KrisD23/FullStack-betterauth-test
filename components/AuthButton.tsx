3c5d475410d47e9e1969490d187d5504886c835e
KrisD23 <krishandwive9@gmail.com>
2026-02-05 10:05:30 +0530

    create the logic for better auth github sign in

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -1,9 +1,25 @@
 "use client";
-import { Github } from "lucide-react";
+import { Github, Loader2 } from "lucide-react";
 import { Button } from "./ui/button";
+import { signIn, useSession } from "@/lib/auth-client";
 
 const AuthButton = () => {
-  const handleGithubSignIn = () => {};
+  const { data: session, isPending } = useSession();
+
+  if (isPending) {
+    return (
+      <div className="flex items-center gap-2 text-muted-foreground">
+        <Loader2 className="w-4 h-4 animate-spin" />
+        Loading....
+      </div>
+    );
+  }
+
+  const handleGithubSignIn = async () => {
+    await signIn.social({
+      provider: "github",
+    });
+  };
 
   return (
     <Button onClick={handleGithubSignIn}>
--- lib/auth.ts
+++ lib/auth.ts
@@ -7,4 +7,11 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql", // or "mysql", "postgresql", ...etc
   }),
+
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    },
+  },
 });
